564c8ee9ae9e3cd4bcebc4a940671c276eb059dc
Brandon <brandon.ebarnett@gmail.com>
2022-08-30 17:14:01 -0400

    Add project files.

---
+++ Exercise012/Program.cs
@@ -0,0 +1,22 @@
+﻿/* Write a program to ask the user to enter 5 numbers.
+ * If a number has been previously entered, display an error message that asks the user to try again.
+   Once the user successfully enters 5 unique numbers, sort them and display the result on the console.*/
+
+List<int> numbers = new List<int>();
+
+while (numbers.Count < 5)
+{
+    Console.WriteLine("Please enter a unique number.");
+    int userInput = Convert.ToInt32(Console.ReadLine());
+
+    if (numbers.Contains(userInput))
+    {
+        Console.WriteLine("You have already entered that value.  Please try again.");
+        continue;
+    }
+    numbers.Add(userInput);
+}
+numbers.Sort();
+
+
+Console.WriteLine(@"You successfully entered 5 unique values: {0}.", string.Join(", ", numbers));
